8178cdc5d6ca2d6fb3f70e9834d7dbd5628a116f
TwishaJain-12 <twisha122124@gmail.com>
2025-09-02 08:26:28 +0530

    LA 2

--- Assignment2/Q6a.cpp
+++ Assignment2/Q6a.cpp
@@ -1,4 +1,3 @@
-//Sparse Matrix: TRANSPOSE
 #include<iostream>
 using namespace std;
 int main(){
@@ -18,8 +17,8 @@ int main(){
             }
         }
     }
-    int t[nz][3], tt[nz][3];
-    int k=0;
+    int t[nz+1][3], tt[nz+1][3];
+    int k=1;
     cout<<endl<<"The matrix is: "<<endl;
     for(int i=0; i<r; i++){
         for(int j=0; j<c; j++){
@@ -34,26 +33,35 @@ int main(){
         cout<<endl;
     }
     cout<<endl;
+    t[0][0]=r;
+    t[0][1]=c;
+    t[0][2]=nz;
 
     //print sparse
     cout<<"Sparse matrix is: "<<endl;
-    for(int i=0; i<nz; i++){
+    for(int i=0; i<=nz; i++){
         cout<<t[i][0]<<"  "<<t[i][1]<<"  "<<t[i][2]<<endl;
     }
 
-    cout<<endl<<"The transpose of sparse matrix is: "<<endl;
-    k=0;
-    for(int i=0; i<c; i++){
-        for(int j=0; j<r; j++){
-            if(m[j][i]!=0){
-                tt[k][0]=i;
-                tt[k][1]=j;
-                tt[k][2]=m[j][i];
-                cout<<tt[k][0]<<"  "<<tt[k][1]<<"  "<<tt[k][2]<<endl; //transpose sparse
+    int j=0;
+    k=1;
+    while(j<c){
+        for(int i=1; i<=nz; i++){
+            if(t[i][1]==j){
+                tt[k][0]=t[i][1]; //col
+                tt[k][1]=t[i][0]; //row
+                tt[k][2]=t[i][2]; //val
                 k++;
             }
         }
+        j++;
     }
-
+    tt[0][0]=r;
+    tt[0][1]=c;
+    tt[0][2]=nz;
+    cout<<endl<<"The transpose of sparse matrix is: "<<endl;
+    for(int i=0; i<=nz; i++){
+        cout<<tt[i][0]<<"  "<<tt[i][1]<<"  "<<tt[i][2]<<endl;
+    }
     return 0;
 }
