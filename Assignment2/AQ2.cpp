e7ff897a15692e9d9fcbadfa6e2320e3dba1123f
TwishaJain-12 <twisha122124@gmail.com>
2025-09-14 20:58:32 +0530

    AQ2 added

--- Assignment2/AQ2.cpp
+++ Assignment2/AQ2.cpp
@@ -1,14 +1,34 @@
 //String Split, string:lowercase English alphabets, split in 3 substrings where one is substring of other 2
 #include<iostream>
+#include<string>
+#include<cstring>
 using namespace std;
 int main(){
-    int T;
-    cout<<"Enter the no. of Test cases: ";
-    cin>>T;
-    string s[T];
-    for(int i=0; i<T; i++){
-        cout<<"Enter the string"<<i+1<<": ";
-        cin>>s[i];
+    string s;
+    cout<<"Enter the string: ";
+    cin>>s;
+    int l=s.length();
+    int freq[l];
+    for(int i=0; i<l; i++){
+        freq[i]=0;
     }
+
+    int i;
+    for(i=0; i<l; i++){
+        for(int j=0; j<l; j++){
+            if(s.at(i)==s.at(j)){
+                freq[i]=freq[i]+1;
+            }
+        }
+        if(freq[i]>=3){
+            cout<<"YES"<<endl;
+            break;
+        }
+    }
+    if(i==l){
+        cout<<"NO"<<endl;
+    }
+
+    return 0;
     
 }
